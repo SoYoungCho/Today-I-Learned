032ad8c7aa06fbbd80e6a1c02567e8793cae2cc8
Soyoung Cho <38921656+SoYoungCho@users.noreply.github.com>
2020-06-11 05:05:07 +0900

    Update srv.c

--- System Programming/FTP-server/4-1 Access Control/srv.c
+++ System Programming/FTP-server/4-1 Access Control/srv.c
@@ -6,23 +6,28 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 //#include <signal.h>
 
 #define MAX_BUF 20
 #define TRUE 1
+#define FALSE 0
+#define ERROR -1
+#define USER_TEXT ("access.txt")
 
 //define functions
 int log_auth(int connfd);
 int user_match(char *user, char *passwd);
 char *client_info(struct sockaddr_in *client);
+int check_ip(char *client_ip);
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[]){ 
+    char *client_ip;
     int listenfd, connfd;
     struct sockaddr_in server, client;
     int cli_len;
 
-    FILE *fp_checkIP; // File stream to check client's IP
+    //FILE *fp_checkIP; // File stream to check client's IP
 
     ////////////// SERVER Implementation //////////////
     // 1. create socket
@@ -56,12 +61,22 @@ int main(int argc, char *argv[]){
         // 4. accept
         connfd = accept(listenfd, (struct sockaddr *)&client, &cli_len); // accept a connect request from client
 
-        char *client_ip = client_info(&client); // display client ip and port
+        client_ip = client_info(&client); // display client ip and port
 
         // code : check if client's IP accessible
+        if(check_ip(client_ip) == TRUE){
+            write(connfd, "ACCEPTED", strlen("ACCEPTED"));
+            printf("** Client is connected **\n");
+            
+        }
+        else if(check_ip(client_ip) == FALSE){
+            write(connfd, "REJECTION", strlen("REJECTION"));
+            printf("** It is NOT authenticated client **\n");
+        }
 
+        getchar();
         
-        
+        /*
         if(log_auth(connfd) == 0){ // if 3 time fail (ok : 1, fail : 0)
             printf("** Fail to log-in **\n");
             close(connfd);
@@ -69,8 +84,10 @@ int main(int argc, char *argv[]){
         }
         printf("** Success to log-in **\n");
         close(connfd);
-        
+        */
     }
+
+    return 0;
 }
 
 int log_auth(int connfd){
@@ -118,3 +135,57 @@ char *client_info(struct sockaddr_in *client){
     printf("- Port :    %d\n", client_port);
     return client_ip;
 }
+
+ int check_ip(char *client_ip){
+    char buf[MAX_BUF];
+    char *pStr;
+    int flag = FALSE;
+
+    FILE *fp_checkIP; // File stream to check client's IP
+
+    fp_checkIP = fopen(USER_TEXT, "r"); // read access.txt file
+    if(fp_checkIP != NULL) {
+        char *pStr;
+        char *cmptokens[4];
+        cmptokens[0] = strtok(client_ip, ".");
+        cmptokens[1] = strtok(NULL, ".");
+        cmptokens[2] = strtok(NULL, ".");
+        cmptokens[3] = strtok(NULL, ".");
+
+        while(!feof(fp_checkIP)){
+            pStr = fgets( buf, sizeof(buf), fp_checkIP );
+            printf( "%s", pStr );
+            char *tokens[4];
+            tokens[0] = strtok(pStr, ".");
+            tokens[1] = strtok(NULL, ".");
+            tokens[2] = strtok(NULL, ".");
+            tokens[3] = strtok(NULL, "\n");
+
+            for(int i = 0; i<4; i++){
+                if(!strcmp(tokens[i], cmptokens[i])){ // same token
+                    flag = TRUE;
+                    continue;
+                }
+                else if(!strcmp(tokens[i],"*")){
+                    flag = TRUE;
+                    continue;
+                }
+                else{
+                    flag = FALSE;
+                    break;
+                }
+            }
+
+            if(flag){
+                fclose( fp_checkIP );
+                return TRUE;
+            }
+
+            getchar();
+        }
+        
+    }
+    fclose( fp_checkIP );
+    return FALSE;
+}
+
